770dcd2137fe622d43f36777d9d386669b13b1dd
kmusau <kelvin.musau@cellulant.io>
2023-04-19 09:04:05 +0300

    update song title - presentation

--- src/main/java/kmusau/jpademo/service/MusicService.java
+++ src/main/java/kmusau/jpademo/service/MusicService.java
@@ -2,11 +2,13 @@ package kmusau.jpademo.service;
 
 import kmusau.jpademo.model.Music;
 import kmusau.jpademo.repository.MusicRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class MusicService {
     @Autowired
@@ -17,6 +19,14 @@ public class MusicService {
     }
 
     public Music addSong(Music music) {
+        return musicRepo.save(music);
+    }
+
+    public Music updateName(Music musicObj, int musicId) {
+        log.info("MusicObj :: {}", musicObj);
+        var musicOpt = musicRepo.findById(musicId);
+        var music = musicOpt.get();
+        music.setSongTitle(musicObj.getSongTitle());
         return musicRepo.save(music);
     }
 }
